74470feb37e36796b96a667d2294f0bbe69f94af
Jadiê Oliveira <jadieoliveira00@gmail.com>
2021-09-05 22:10:32 -0300

    Init 100 days of swiftUi

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,21 +1,59 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Jadiê on 05/09/21.
-//
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+ //
+ //  ContentView.swift
+ //  WeSplit
+ //
+ //  Created by Jadiê on 05/09/21.
+ //
+ 
+ import SwiftUI
+ 
+ struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerson = grandTotal / peopleCount
+        
+        return amountPerson
     }
-}
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Amount", text: $checkAmount).keyboardType(.numberPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) people")
+                    }
+                }
+                Section(header:Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count ) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Text("$ \(totalPerPerson)")
 
-struct ContentView_Previews: PreviewProvider {
+            }
+            .navigationBarTitle(Text("WeSplit"))
+        }
+    }
+ }
+ 
+ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+ }
